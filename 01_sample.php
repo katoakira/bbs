644b7d99b14a8e6980566e1043b7d5ca05adaa1c
Akira <akira.kato.19930831@icloud.com>
2014-11-30 17:37:55 +0800

    first commit

---
+++ 01_sample.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://wwww.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charaset=utf-8" />
+	<title>Hello, World</title>
+</head>
+<body>
+	<p>Hello, World</p>
+</body>
+</html>
